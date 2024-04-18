053a6870752cee0d89fb5cb92afc1712c7fd7a25
Praveen Vuddagiri <praveen.vuddagiri@resmed.co.in>
2024-04-18 16:11:28 +0530

    added dark light theme and button component

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,20 +1,37 @@
-"use client";
+"use client"
 
-import { ReactNode } from "react";
-
-interface ButtonProps {
-  children: ReactNode;
-  className?: string;
-  appName: string;
+interface ButtonPropType {
+  variant: "primary" | "secondary" | "textButton";
+  size: "sm" | "md" | "lg";
+  buttonText: string;
+  [key: string]: any;
 }
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
+const variantStyles = {
+  primary:
+    "bg-[#18181b] text-white rounded-md dark:text-[#18181b] dark:bg-[#FAFAFA] hover:bg-[#18181b]/80",
+  secondary:
+    "bg-transparent border-[1px] text-[#18181b] rounded-md dark:text-[#FAFAFA] dark:bg-transparent",
+  textButton:
+    "bg-transparent text-[#18181b] dark:text-[#FAFAFA] dark:bg-transparent",
+};
+
+const sizeStyles = {
+  sm: "px-2.5 py-0.5 text-xs",
+  md: "px-4 py-1 text-sm",
+  lg: "px-6 py-2 text-base",
+};
+const Button = (props: ButtonPropType) => {
   return (
-    <button
-      className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
+    <div
+      className={`${variantStyles[props.variant]} ${
+        sizeStyles[props.size]
+      } cursor-pointer w-fit`}
+      {...props}
     >
-      {children}
-    </button>
+      {props.buttonText}
+    </div>
   );
 };
+
+export default Button;
