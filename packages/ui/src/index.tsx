73bb05904211532ba081a90640aa2efc0ce48073
Praveen Vuddagiri <vuddagiripraveen@gmail.com>
2024-04-30 17:24:33 +0530

    added alert box

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -5,6 +5,7 @@ export * from "./components/providers";
 // export * from './button';
 export * from './DataTable';
 export * from './components'
+export * from './deletePopupComp';
 
 // lib
 export * from "./lib/utils";
