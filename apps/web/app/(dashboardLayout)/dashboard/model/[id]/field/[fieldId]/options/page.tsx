e19e6e481b331a4cfc2cbcd97d11f99fc3255196
Avinash Koduri <saiavinashkoduri@gmail.com>
2024-05-07 15:50:56 +0530

    changes in params and headers

--- apps/web/app/(dashboardLayout)/dashboard/model/[id]/field/[fieldId]/options/page.tsx
+++ apps/web/app/(dashboardLayout)/dashboard/model/[id]/field/[fieldId]/options/page.tsx
@@ -6,9 +6,15 @@ import { FieldOptionsData } from '../../../../../../../../tempData';
 
 const page = ({params}: {params: {fieldId: string}}) => {
     console.log(params.fieldId)
+//     const url = "http://localhost:3000/dashboard/model/4/field/111/options/add";
+// const parts = url.split("/"); // Split the URL by '/'
+// const modelIndex = parts.indexOf("model"); // Find the index of "model"
+// const modelId = parts[modelIndex + 1]; // Get the next part after "model"
+
+// console.log(modelId);
     return (
         <div>
-            <DataTable columns={fieldOptionsColumns} data={FieldOptionsData} />
+            <DataTable columns={fieldOptionsColumns} data={FieldOptionsData} url="options/add" text="Create field options"/>
 
         </div>
     )
