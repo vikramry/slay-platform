35660580738df3efe7cfd4f0c01e221b17044aa2
Praveen Vuddagiri <vuddagiripraveen@gmail.com>
2024-05-11 11:24:55 +0530

    sidebar fixed

--- packages/ui/src/sideBar.tsx
+++ packages/ui/src/sideBar.tsx
@@ -74,61 +74,67 @@
 
 "use client"
 import Link from "next/link";
-import { usePathname } from 'next/navigation';
+import { usePathname, useParams } from 'next/navigation';
 
 export interface SideBarProps {
   title: string; link: string;
 }
 
-const data: Record<string, SideBarProps[]> = {
-  "default": [
-    {
-      title: "Models",
-      link: "dashboard/model"
-    },
-    {
-      title: "Components",
-      link: "dashboard/components"
-    },
-    {
-      title: "Users",
-      link: "dashboard/users"
-    },
-    {
-      title: "Tabs",
-      link: "dashboard/tabs"
-    },
-  ],
-  "model": [
-    {
-      title: "Fields",
-      link: "dashboard/model"
-    },
-    {
-        title: "Options",
-        link: "options"
-      },
-    {
-      title: "Permissions",
-      link: "dashboard/model/permissions"
-    },
-    {
-      title: "Functions",
-      link: "dashboard/model/functions"
-    },
-    {
-      title: "Layouts",
-      link: "dashboard/model/layouts"
-    },
-  ]
-};
 
 export function SideBar() {
+  const data: Record<string, SideBarProps[]> = {
+    "default": [
+      {
+        title: "Models",
+        link: "dashboard/model"
+      },
+      {
+        title: "Components",
+        link: "dashboard/components"
+      },
+      {
+        title: "Users",
+        link: "dashboard/users"
+      },
+      {
+        title: "Tabs",
+        link: "dashboard/tabs"
+      },
+    ],
+    "model": [
+      {
+        title: "Fields",
+        link: `dashboard/model/${useParams()?.id}`
+      },
+      {
+        title: "Options",
+        link: `dashboard/model/${useParams()?.id}/options`
+      },
+      {
+        title: "Permissions",
+        link: `dashboard/model/${useParams()?.id}/permissions`
+      },
+      {
+        title: "Functions",
+        link: `dashboard/model/${useParams()?.id}/functions`
+      },
+      {
+        title: "Layouts",
+        link: `dashboard/model/${useParams()?.id}/layouts`
+      },
+    ]
+  };
+  const sidebarData = data[/\/model\/.+/.test(usePathname()) && !usePathname().includes("model/createModel") ? "model" : "default"]
   return (
     <div className="md:block hidden">
       <div className="flex flex-col gap-2 ">
-        {data[usePathname().includes("model") ? "model" : "default"]?.map((item: SideBarProps) => {
-          const isActive = usePathname() === `/${item?.link}`;
+        {sidebarData?.map((item: SideBarProps) => {
+          const link = `/${item?.link}`;
+          const regex = new RegExp(`^${link}(\/|$)`);
+
+          let isActive = regex.test(usePathname());
+          if(item.title === "Fields") 
+            isActive = usePathname().includes(`model/${useParams().id}/options`) ? false : true;
           return (
             <Link href={`/${item?.link}`} key={item?.link}>
               <div className={`p-2 rounded-lg w-[180px] hover:bg-gray-100 hover:text-black dark:hover:bg-gray-100 dark:hover:bg-opacity-25 dark:hover:text-white dark:text-white  flex flex-row gap-2 text-center text-sm align-middle font-medium ${isActive ? "bg-[black] dark:bg-gray-200  text-white dark:text-black dark:bg-opacity-25" : ""} ease-in-out duration-300`}>
