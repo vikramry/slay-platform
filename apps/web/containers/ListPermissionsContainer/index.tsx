70de790035695c7576c3ce8bc778affb9d79ab98
Praveen Vuddagiri <vuddagiripraveen@gmail.com>
2024-05-15 19:39:26 +0530

    added permissions default listing of fields

--- apps/web/containers/ListPermissionsContainer/index.tsx
+++ apps/web/containers/ListPermissionsContainer/index.tsx
@@ -3,13 +3,15 @@ import { serverFetch } from "@/app/action";
 import { useLazyQuery } from "@/app/hook";
 import { getlistmodelfields } from "@/app/queries";
 import { useParams } from "next/navigation";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { DataTable } from "@repo/ui";
 import { permissionColumns } from "@/app/(dashboardLayout)/dashboard/columns";
+import { ModelFieldType, PermissionType } from "@/types";
 
 const ListPermissionContainer = () => {
   const [getAllFields, { data, error, loading }] = useLazyQuery(serverFetch);
   const { id } = useParams();
+  const [modelFields, setModelFields] = useState<PermissionType[]>([]);
 
   useEffect(() => {
     getAllFields(
@@ -28,25 +30,30 @@ const ListPermissionContainer = () => {
   }, []);
 
   useEffect(() => {
-    if(error){
-        
+    if (error) {
+
+    }
+    if (data) {
+      const modifiedFields = data?.listModelFields?.docs.map((field: ModelFieldType) => {
+        return {
+          id: field.id,
+          modelField: field,
+          create: true,
+          read: true,
+          delete: true,
+          update: true,
+        }
+      })
+
+      setModelFields(modifiedFields);
     }
   }, [data, error, loading]);
   return (
     <div>
       <DataTable
         columns={permissionColumns}
-        data={[{
-            id: "123qww",
-            modelField: {
-                label: "Permission"
-            },
-            create: false,
-            update: true,
-            read: true,
-            delete: true
-        }]}
-        filterBy={"modelField.label"}
+        data={modelFields || []}
+        filterBy={""}
         text={"Edit"}
         url={"#"}
       />
