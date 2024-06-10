9523ddaed534ec55379b7e2c186fa19ec50c3321
Avinash Koduri <saiavinashkoduri@gmail.com>
2024-06-10 18:17:12 +0530

    changes in layout removed border left

--- apps/web/app/(dashboardLayout)/layout.tsx
+++ apps/web/app/(dashboardLayout)/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
         <div className="border-[1px] m-[20px] rounded-lg h-[calc(100vh-110px)] dark:border-gray-700 ">
           <div className="flex flex-row gap-3 p-[10px] w-full">
             <SideBar />
-            <div className={`md:w-[${router === "/dashboard" ?"calc(100vw-215px)":"calc(100vw-265px)"}] w-[calc(100vw-50px)] h-[calc(100vh-130px)] overflow-y-auto dark:border-gray-700 border-l-[1px] px-2`}>
+            <div className={`md:w-[${router === "/dashboard" ?"calc(100vw-215px)":"calc(100vw-265px)"}] w-[calc(100vw-50px)] h-[calc(100vh-130px)] overflow-y-auto dark:border-gray-700 ${router !== "/dashboard" && "border-l-[1px]"} px-2`}>
               {children}
             </div>
           </div>
