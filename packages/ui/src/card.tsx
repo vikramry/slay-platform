84f599c121d39960c2e1ecf19d57a224ebd3b1c2
Avinash Koduri <saiavinashkoduri@gmail.com>
2024-07-08 10:33:35 +0530

    added loaders and changes in navbar

--- packages/ui/src/card.tsx
+++ packages/ui/src/card.tsx
@@ -6,7 +6,7 @@ interface cardprops {
 const Card = ({ children, classNames }: cardprops) => {
   return (
     <div
-      className={`${classNames} dark:bg-gray-700 border-[1px] border-[#E5E5E5] dark:border-[#6b7280]  dark:text-white w-full h-full rounded-lg `}
+      className={`${classNames} dark:bg-gray-700 border-[1px] border-[#E5E5E5] dark:border-[#6b7280]  dark:text-white w-full h-full rounded-lg flex flex-col justify-center items-center p-5`}
     >
       {children}
     </div>
--- packages/ui/src/navBar.tsx
+++ packages/ui/src/navBar.tsx
@@ -1,5 +1,5 @@
+import { useRouter, useParams, usePathname } from "next/navigation";
 
-import { useRouter,useParams,usePathname } from "next/navigation";
 export interface LIST_TABSTYPES {
   label: string;
   id: string;
@@ -25,33 +25,43 @@ export function NavBar({
   const { modelName } = useParams();
   const pathName = usePathname();
 
-  console.log(modelName,"mdelName")
-    return (
-    <div className={`flex flex-row h-[70px]  items-center  ${pathName === "/dashboard" || pathName.includes('/dashboard/o/') ? "block" : "hidden"}`}>
-      {/* <div className="rounded-full w-[50px] h-[50px] border-gray border-[1px] mr-5 dark:border-gray-500"></div>
-      <div className="rounded-full w-[50px] h-[50px] border-gray border-[1px] mr-5 dark:border-gray-500"></div>
-      <div className="rounded-full w-[50px] h-[50px] border-gray border-[1px] mr-5 dark:border-gray-500"></div> */}
+  console.log(modelName, "modelName");
+  return (
+    <div
+      className={`flex flex-row h-[70px] items-center ${
+        pathName === "/dashboard" || pathName.includes("/dashboard/o/")
+          ? "block"
+          : "hidden"
+      }`}
+    >
       <div className="rounded-full w-[calc(100vw-20px)] h-[36px] border-gray border-[1px] dark:border-gray-500 flex flex-row justify-center items-center align-middle overflow-x-auto px-4">
         <div className="md:block hidden">
           <div className="flex flex-row justify-center gap-8 ">
             {loading ? (
-              <h4
-                className={`text-gray-500 text-[14px] hover:text-black dark:hover:text-white font-semibold ease-in-out duration-300`}
-              >
-                Loading...
-              </h4>
+              // Skeleton Loader
+              <>
+                {[1, 2, 3, 4, 5].map((_, index) => (
+
+                  <div   key={index} className="h-[16px] w-[100px]  bg-gray-200 rounded-full dark:bg-gray-700 "></div>
+                ))}
+              </>
             ) : (
               <>
                 {tabsData?.map((item) => {
                   return (
-                    // <Link href={"#"} key={item.label}>
                     <h4
-                      className={`${modelName == item?.model?.name ? "text-black dark:text-white font-bold" : "text-[#7B7B7D] font-[500px]"} text-[16px] hover:text-black hover:font-bold cursor-pointer dark:hover:text-white  ease-in-out duration-300`}
-                      onClick={() => router.push(`/dashboard/o/${item.model.name}/list`)}
+                      key={item.label}
+                      className={`${
+                        modelName == item?.model?.name
+                          ? "text-black dark:text-white font-bold"
+                          : "text-[#7B7B7D] font-[500px]"
+                      } text-[16px] hover:text-black hover:font-bold cursor-pointer dark:hover:text-white ease-in-out duration-300`}
+                      onClick={() =>
+                        router.push(`/dashboard/o/${item.model.name}/list`)
+                      }
                     >
                       {item?.label}
                     </h4>
-                    // </Link>
                   );
                 })}
               </>
