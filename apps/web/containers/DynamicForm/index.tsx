aa536972d964e0d0597138c10d87585a4b09dc18
Avinash Koduri <saiavinashkoduri@gmail.com>
2024-07-31 19:22:37 +0530

    added zod resolver

--- apps/web/containers/DynamicForm/index.tsx
+++ apps/web/containers/DynamicForm/index.tsx
@@ -1,36 +1,55 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm, Controller, SubmitHandler } from "react-hook-form";
+import { useForm, SubmitHandler } from "react-hook-form";
 import { z } from "zod";
-import { Button, Input, Checkbox, Select, Form, FormItem, FormLabel, FormControl, FormMessage, FormField } from "@repo/ui";
-// import { Toaster, toast } from "react-hot-toast";
+import {
+  Button, Input, Checkbox, Form, FormItem, FormLabel, FormControl, FormMessage, FormField,
+} from "@repo/ui";
 
-// Define a schema based on your metadata
-const formSchema = z.object({
-  firstName: z.string().nonempty("First name is required"),
-  lastName: z.string().nonempty("Last name is required"),
-  age: z.coerce.number().min(1, "Age must be at least 1"),
-  email: z.string().email("Invalid email address"),
-  isAdmin: z.boolean(),
-});
-
-type FormSchema = z.infer<typeof formSchema>;
+interface FieldMetadata {
+  name: string;
+  type: "text" | "number" | "checkbox" ;
+  label: string;
+}
 
 interface DynamicFormProps {
   metadata: FieldMetadata[];
 }
 
+const generateSchema = (metadata: FieldMetadata[]) => {
+  const schemaObj: Record<string, any> = {};
+
+  metadata.forEach((field) => {
+    switch (field.type) {
+      case "text":
+        schemaObj[field.name] = z.string().optional();
+        break;
+      case "number":
+        schemaObj[field.name] = z.coerce.number().optional();
+        break;
+
+      case "checkbox":
+        schemaObj[field.name] = z.boolean();
+        break;
+      default:
+        break;
+    }
+  });
+
+  return z.object(schemaObj);
+};
+
 const DynamicForm: React.FC<DynamicFormProps> = ({ metadata }) => {
+  const formSchema = generateSchema(metadata);
+  type FormSchema = z.infer<typeof formSchema>;
+
   const form = useForm<FormSchema>({
     resolver: zodResolver(formSchema),
-    defaultValues: {
-      firstName: "",
-      lastName: "",
-      age: 0,
-      email: "",
-      isAdmin: false,
-    },
+    defaultValues: metadata.reduce((acc, field) => {
+      acc[field.name] = field.type === "checkbox" ? false : field.type == "number" ? 0:"";
+      return acc;
+    }, {} as Record<string, any>),
   });
 
   const onSubmit: SubmitHandler<FormSchema> = (values) => {
@@ -40,78 +59,72 @@ const DynamicForm: React.FC<DynamicFormProps> = ({ metadata }) => {
 
   return (
     <div>
-
-    <Form {...form}>
-      {/* <Toaster /> */}
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <div className="grid md:grid-cols-2 grid-cols-1 gap-5">
-          {metadata.map((item) => {
-            console.log(item.label,"field")
-            return(
-<div>
-{item.type === "text" && (
+      <Form {...form}>
+        {/* <Toaster /> */}
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <div className="grid md:grid-cols-2 grid-cols-1 gap-5">
+            {metadata.map((item) => (
+              <div key={item.name}>
+                {item.type === "text" && (
                   <FormField
-                  control={form.control}
-                  name={item.name}
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>{item.label}</FormLabel>
-                      <FormControl>
-                        <Input placeholder={item.name} {...field} type="text" />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
+                    control={form.control}
+                    name={item.name}
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>{item.label}</FormLabel>
+                        <FormControl>
+                          <Input placeholder={item.label} {...field} type="text" />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
                 )}
                 {item.type === "number" && (
                   <FormField
-                  control={form.control}
-                  name={item.name}
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>{item.name}</FormLabel>
-                      <FormControl>
-                        <Input placeholder={item.name} {...field} type="number" />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
+                    control={form.control}
+                    name={item.name}
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>{item.label}</FormLabel>
+                        <FormControl>
+                          <Input placeholder={item.label} {...field} type="number" />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
                 )}
 
                 {item.type === "checkbox" && (
                   <FormField
-                  control={form.control}
-                  name={item.name}
-                  render={({ field }) => (
+                    control={form.control}
+                    name={item.name}
+                    render={({ field }) => (
                       <FormItem className="flex flex-row items-center space-x-3 space-y-0 ">
-                          <FormControl>
-                              <Checkbox
-                                  checked={field.value}
-                                  onCheckedChange={field.onChange}
-                              />
-                          </FormControl>
-                          <div className="space-y-1 leading-none">
-                              <FormLabel>{item.label}</FormLabel>
-                          </div>
+                        <FormControl>
+                          <Checkbox
+                            checked={field.value}
+                            onCheckedChange={field.onChange}
+                          />
+                        </FormControl>
+                        <div className="space-y-1 leading-none">
+                          <FormLabel>{item.label}</FormLabel>
+                        </div>
                       </FormItem>
-                  )}
-              />
+                    )}
+                  />
                 )}
-</div>
-              
-          )}
-           
-          )}
-        </div>
-        <div className="flex justify-center items-center">
-          <Button type="submit" variant="default" className="flex justify-center items-center w-fit">
-            Submit
-          </Button>
-        </div>
-      </form>
-    </Form>
+              </div>
+            ))}
+          </div>
+          <div className="flex justify-center items-center">
+            <Button type="submit" variant="default" className="flex justify-center items-center w-fit">
+              Submit
+            </Button>
+          </div>
+        </form>
+      </Form>
     </div>
   );
 };
