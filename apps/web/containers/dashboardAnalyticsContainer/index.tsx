7f52658afa9023d3399e0ef85ed7178ff56dca1e
Manikanta <kurapatimanikanta66@gmail.com>
2024-12-10 15:12:13 +0530

    added quary for analytics

--- apps/web/containers/dashboardAnalyticsContainer/index.tsx
+++ apps/web/containers/dashboardAnalyticsContainer/index.tsx
@@ -1,6 +1,34 @@
-import React from 'react'
+'use client'
+import { serverFetch } from '@/app/action';
+import { useLazyQuery } from '@/app/hook';
+import { DASHBOARD_ANALYTICS } from '@/app/queries';
+import React, { useEffect } from 'react'
 
 function DashboardAnalyticsContainer() {
+
+  const [getDashboardData, getDashboardDataResponse] = useLazyQuery(serverFetch);
+  useEffect(() => {
+    getDashboardData(
+      DASHBOARD_ANALYTICS,
+      {
+      
+      },
+      {
+        cache: "no-store",
+      }
+    );
+  }, []);
+  useEffect(() => {
+    if (getDashboardDataResponse?.data) {
+      console.log(getDashboardDataResponse?.data, "getDashboardDataResponse");
+    } else if (getDashboardDataResponse?.error) {
+      console.log(getDashboardDataResponse?.error, "getDashboardDataResponse");
+    }
+  }, [
+    getDashboardDataResponse?.data,
+    getDashboardDataResponse?.error,
+    getDashboardDataResponse?.loading,
+  ]);
   return (
     <div>DashboardAnalyticsContainer</div>
   )
